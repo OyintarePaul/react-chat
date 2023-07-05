d4752a55ebd1ccb2f3daa7007d9aac6d0edcd648
Egrenbido Oyintare Paul <73933865+OyintarePaul@users.noreply.github.com>
2023-07-05 14:25:54 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, {
-    createElement as r,
-    useEffect,
-    createContext,
+  createElement as r,
+  useEffect,
+  createContext,
 } from "https://cdn.skypack.dev/react";
 import { RouterProvider } from "https://cdn.skypack.dev/react-router-dom";
 
 import router from "./components/MainRoutes.js";
 import AuthProvider from "./contexts/authContext.js";
-function App(props) {
-    return r(AuthProvider, null, r(RouterProvider, { router }));
+function App() {
+  return r(
+    AuthProvider,
+    null,
+    r(RouterProvider, {
+      router,
+    })
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React, { createElement as r } from "https://cdn.skypack.dev/react";
 import ReactDOM from "https://cdn.skypack.dev/react-dom";
-
 import App from "./App.js";
-
+console.log("Started App");
 const root = ReactDOM.render(r(App), document.getElementById("root"));
