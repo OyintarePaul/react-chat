e5948f1abdc6d230ca0c653085209e3c4ed77c93
Egrenbido Oyintare Paul <73933865+OyintarePaul@users.noreply.github.com>
2023-07-05 14:28:46 +0100

    Add files via upload

--- src/contexts/authContext.js
+++ src/contexts/authContext.js
@@ -1,9 +1,9 @@
 import {
-    createElement as r,
-    createContext,
-    useContext,
-    useState,
-    useEffect,
+  createElement as r,
+  createContext,
+  useContext,
+  useState,
+  useEffect,
 } from "https://cdn.skypack.dev/react";
 
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-auth.js";
@@ -12,16 +12,19 @@ import { auth } from "../server/firebase.js";
 const authContext = createContext();
 
 function useAuth() {
-    return useContext(authContext);
+  return useContext(authContext);
 }
 
 function AuthProvider({ children }) {
-    const [user, setUser] = useState(auth);
-    onAuthStateChanged(auth, (user) => {
-        setUser(user.auth);
-    });
+  const [user, setUser] = useState(auth);
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      return setUser(user.auth);
+    }
+    setUser(null);
+  });
 
-    return r(authContext.Provider, { value: user }, children);
+  return r(authContext.Provider, { value: user }, children);
 }
 export { useAuth };
 export default AuthProvider;
